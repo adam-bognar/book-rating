a97cd0e02f994095538921d3c978e3a81ae6ba62
Ádám Bognár <bgnr.adam@gmail.com>
2025-08-28 20:54:27 +0200

    Add new routes for main app pages
    Added routes for Home, Calendar, Profile, Book (with id), and Signup pages. Set default route to redirect to Home. This expands navigation options in the application.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { Signin } from './pages/signin/signin';
+import { Signup } from './pages/signup/signup';
+import { Home } from './pages/home/home';
+import { Calendar } from './pages/calendar/calendar';
+import { Profile } from './pages/profile/profile';
+import { Book } from './pages/book/book';
 
 export const routes: Routes = [
-    { path: 'signin', component: Signin }
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: Home },
+    { path: 'calendar', component: Calendar },
+    { path: 'profile', component: Profile },
+    { path: 'book/:id', component: Book },
+    { path: 'signin', component: Signin },
+    { path: 'signup', component: Signup }
 ];
